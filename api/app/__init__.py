ef8e86ab4c6713baf22f3a42563c3ff1d2ac3fb0
Eduardo Salazar <eduardosalazar89@hotmail.es>
2020-10-31 16:33:29 -0600

    Configure database
    [add]
    flast-sqlalchemy and it's config variables 
    
    [update]
    initialize database in app __init__.py

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 # Import blueprints
 from app.info.routes import info
@@ -7,6 +8,7 @@ from app.users.routes import users
 # Configure app
 app = Flask(__name__)
 app.config.from_object('app.settings.Config')
+db = SQLAlchemy(app)
 
 # Register blueprints
 app.register_blueprint(info)
--- api/app/settings.py
+++ api/app/settings.py
@@ -14,3 +14,6 @@ class Config:
     APP_NAME = environ.get("APP_NAME", "Feedma Drive")
     APP_VERSION = environ.get("APP_VERSION", "0.0.1")
 
+    # Third party variables
+    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI", "sqlite:////tmp/test.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = environ.get("SQLALCHEMY_TRACK_MODIFICATIONS", False)
