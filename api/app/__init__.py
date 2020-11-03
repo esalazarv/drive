2276b558b62dd4c58db4f2a73a436f1fdcf7c5c7
Eduardo Salazar <eduardosalazar89@hotmail.es>
2020-11-03 17:42:21 -0600

    Add migration configuration
    [add]
    flask-migrate
    generate first migration files
    add FLASK_APP environment var in .env.example

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def application():
     # Configure app
     app = Flask(__name__)
     app.config.from_object('app.settings.Config')
     db.init_app(app)
+    migrate.init_app(app, db)
 
     # Import blueprints
     from app.info.routes import info
