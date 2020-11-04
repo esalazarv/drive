b6904957b9acb88040fa2d62e7fb198435a90441
Eduardo Salazar <eduardosalazar89@hotmail.es>
2020-11-04 02:51:45 -0600

    Fix on user model

--- api/app/users/models.py
+++ api/app/users/models.py
@@ -6,5 +6,5 @@ class User(db.Model):
     id = db.Column(db.String(255), primary_key=True, default=lambda: uuid.uuid4().hex)
     username = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255))
-    created_at = db.Column(db.DateTime(), default=lambda: datetime.datetime.utcnow)
-    updated_at = db.Column(db.DateTime(), default=lambda: datetime.datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=lambda: datetime.datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=lambda: datetime.datetime.utcnow)
